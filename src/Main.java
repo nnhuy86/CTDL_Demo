9022b2889de3e995e4bf0a54484f3b231ead7860
HTT <nguyenhuy.86nt@gmail.com>
2019-11-08 08:23:42 +0700

    Task2_loadDuLieu: prototype for loadDuLieu

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,46 @@
 
 public class Main {
-	public static void loadDuLieu()
-	{
-		
-	}
+    public static void layCauHoiTuFile()
+    {
 
-	public static void dangNhap()
-	{
-		
-	}
-	
-	public static void ketThucChuongTrinh()
-	{
-		
-	}
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		loadDuLieu();
-		dangNhap();
-		ketThucChuongTrinh();
-	}
+    }
+
+    public static void layMonHocTuFile()
+    {
+
+    }
+
+    public static void layDanhSachLopTuFile()
+    {
+
+    }
+
+    public static void loadDuLieu()
+    {
+        layCauHoiTuFile();
+
+        layMonHocTuFile();
+
+        layDanhSachLopTuFile();
+
+    }
+
+
+    public static void dangNhap()
+    {
+
+    }
+
+    public static void ketThucChuongTrinh()
+    {
+
+    }
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        loadDuLieu();
+        dangNhap();
+        ketThucChuongTrinh();
+    }
 
 }
